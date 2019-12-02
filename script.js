664402f674e14daa98d5f22464b91c2f5897bcab
Daniel Åslund <daniel.aaslund@gmail.com>
2019-12-02 13:48:39 +0100

    Login features based off of JSON file

---
+++ script.js
@@ -0,0 +1,38 @@
+$("document").ready(function() {
+    $("#loginForm").submit(function(event) {
+        event.preventDefault();
+        SignIn($(this));
+    });
+});
+
+function SignIn(form) {
+
+    var inputs = form.find(":input");
+
+    var userName = inputs[0].value;
+    var password = inputs[1].value;
+
+    var users;
+
+    $.ajax({
+        cache: false,
+        url: "Store/users.txt",
+        success: function(response) {
+            users = JSON.parse(response);
+        }
+    }).then(function() {
+        
+        $.each(users, function(index, element) {
+            if (element.userName == userName) {
+                if (element.password == password) {
+                    console.log("logged in");
+                    return false;
+                }
+                else {
+                    console.log("incorrect password");
+                    return false;
+                }
+            }
+        });
+    });
+}
