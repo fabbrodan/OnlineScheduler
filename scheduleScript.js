13411d2cab132a9330e5ec7171371f77b433c0bf
Daniel Åslund <daniel.aaslund@gmail.com>
2019-12-04 11:38:22 +0100

    Added firebase logon and appointment saving functionality

--- scheduleScript.js
+++ scheduleScript.js
@@ -1,17 +1,23 @@
 $("document").ready(function() {
+
     var userName = GetUrlParam("user");
-    var JSONOBJ = {
-        "userName": userName,
-        "test1": "test1",
-        "test2": 2
-    }
-    console.log(JSON.stringify(JSONOBJ));
-    $.ajax({
-        type: "GET",
-        url: "127.0.0.6/save_json.php",
-        data: { data: JSON.stringify(JSONOBJ) },
-        success: console.log("saved!")
-    });
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyAh50GpB6sirjKPLqY-I6KAwN2tkfR86rs",
+        authDomain: "onlinescheduler-cc7ee.firebaseapp.com",
+        databaseURL: "https://onlinescheduler-cc7ee.firebaseio.com",
+        projectId: "onlinescheduler-cc7ee",
+        storageBucket: "onlinescheduler-cc7ee.appspot.com",
+        messagingSenderId: "32569569158",
+        appId: "1:32569569158:web:c26fcb132fbd658dd4c607"
+    };
+    
+    var fb = firebase.initializeApp(firebaseConfig);
+    $("#formApointment").submit(function(event) {
+        event.preventDefault();
+        SaveAppointment(fb.database(), $(this), userName);
+    })
+
 });
 
 function GetUrlParam(sParam) {
@@ -25,4 +31,34 @@ function GetUrlParam(sParam) {
 	            return sParameterName[1];
 	        }
 	    }
+}
+
+function AddApointment(){
+    var form = $("#formApointment");
+    form.css("display", "flex");
+    $("#apointmentButton").css("display", "none");
+}
+
+function SaveAppointment(database, form, userName) {
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var notes = $("#inputNotes").val();
+    var from = $("#from").val();
+    var to = $("#to").val();
+
+    var apointment = {
+        aname: name,
+        aemail: email,
+        anotes: notes,
+        afrom: from,
+        ato: to
+    }
+
+    database.ref("/appointments/").set( {
+        user: userName,
+        description: notes,
+        startTime: from,
+        endTime: to
+    });
 }
--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 $("document").ready(function() {
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyAh50GpB6sirjKPLqY-I6KAwN2tkfR86rs",
+        authDomain: "onlinescheduler-cc7ee.firebaseapp.com",
+        databaseURL: "https://onlinescheduler-cc7ee.firebaseio.com",
+        projectId: "onlinescheduler-cc7ee",
+        storageBucket: "onlinescheduler-cc7ee.appspot.com",
+        messagingSenderId: "32569569158",
+        appId: "1:32569569158:web:c26fcb132fbd658dd4c607"
+    };
+    
+    var fb = firebase.initializeApp(firebaseConfig);
+    var database = fb.database();
+
     $("#loginForm").submit(function(event) {
         event.preventDefault();
-        SignIn($(this));
+        SignIn($(this), fb);
     });
 });
 
-function SignIn(form) {
+function SignIn(form, fb) {
     var inputs = form.find(":input");
     var userName = inputs[0].value;
     var password = inputs[1].value;
-    var users;
 
-    $.ajax({
-        cache: false,
-        url: "Store/users.txt",
-        success: function(response) {
-            users = JSON.parse(response);
-        }
-    }).then(function() {
-        
-        $.each(users, function(index, element) {
-            if (element.userName == userName) {
-                if (element.password == password) {
-                    console.log("logged in");
-                    window.location.href="/schedule.html"
-                    return false;
-                }
-                else {
-                    console.log("incorrect password");
-                    return false;
-                }
+    
+
+    fb.database().ref("/users/"+userName).once("value", function(snapshot) {
+        if (snapshot.exists()) {
+            var fbPassword = snapshot.val();
+            if (password === fbPassword["password"]) {
+                window.location.href="/schedule.html?user="+userName;
             }
-        });
-    });
+            else {
+                alert("incorrect password");
+            }
+        }
+        else {
+            alert("incorrect username");
+        }
+    })
 }
